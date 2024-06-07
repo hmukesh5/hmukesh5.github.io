91fe3dd6d93c3ff80d93770cea09eaf1b53ea01b
Hemanth <hmukesh@outlook.com>
2024-06-07 16:15:52 -0500

    added about me
    added a little about me section with a few links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import * as Accordion from '@radix-ui/react-accordion';
 import Project from './components/project.tsx';
+import linkedin from './data/linkedin.png';
+import github from './data/github.png';
 
 function App() {
+    // document.body.style.backgroundColor = "black";
+    // document.body.style.color = "#e4e4e4";
     return (
         <div className="min-h-screen max-w-4xl mx-auto py-8 px-6">
 
-            <div className="mt-4 mb-8">
-                <p className="text-center font-bold text-5xl">Hemanth Mukesh</p>
+            <div className="mt-4 mb-4">
+                <p className="text-center font-bold text-5xl">Hemanth Mukesh</p>                
+            </div>
+
+            <div className="mt-6 mb-6 flex justify-evenly m-auto w-40">
+                <a href="https://linkedin.com/in/hmukesh5" target="_blank"><img src={linkedin} width={30} height={30}></img></a>
+                <a href="https://github.com/hmukesh5" target="_blank"><img src={github} width={30} height={30}></img></a>
+            </div>
+
+            <div className="mb-4 text-lg">
+                <p>Hello! My name is Hemanth Mukesh, and I'm a computer science senior at Texas A&M University. 
+                   Below is a collection of my projects I've worked on both in and out of college.
+                   Feel free to contact me at <a className="underline" href="mailto:hmukesh@outlook.com">hmukesh@outlook.com</a>!</p>
             </div>
 
             <div className="max-w-fit">
