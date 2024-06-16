c84b3f3191805b9464855aef2aada4fcc3eb4da9
Hemanth <hmukesh@outlook.com>
2024-06-15 19:44:18 -0500

    server testing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const { exec } = require('node:child_process');
 
 app.use(express.json())
 
@@ -9,10 +10,26 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/test', (req, res) => {
+    console.log('testing...');
+
+    const command = 'Homework2.exe hmukesh.me  8.8.8.8';
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            res.send(`<h2>Error:</h2><pre>${stderr}</pre>`);
+        } else {
+            res.send(`<h2>Output:</h2><pre>${stdout}</pre>`);
+        }
+    });
+});
+
 app.post('/run', (req, res) => {
+    console.log("processing /run post request...")
     req.body;
     res.json(req.body)
-    // const command = `./your_cpp_program ${args}`;
+    console.log("processed")
+    const command = `./helloworld.exe`;
 
     // exec(command, (error, stdout, stderr) => {
     //     if (error) {
